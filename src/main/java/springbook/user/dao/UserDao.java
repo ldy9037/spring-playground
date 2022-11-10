ccc94ae368d88ecd44892ecd1cce146807e25330
emadam <ldy9037@naver.com>
2022-11-10 12:22:32 +0900

    refactor(UserDao.java): add, deleteAll 메서드에 익명 내부 클래스 구현
    DAO의 메서드 별로 전략을 구현하는 것은 너무 많은 class 생성을 해야하는 단점이 발생함. 때문에 전략을 각 DAO 내부에 익명 내부 클래스로 구현해 Context 메서드를 호출하도록 변경
    
    토비의 스프링 227 - 231

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -20,8 +20,19 @@ public class UserDao {
     }
 
     public void add(User user) throws SQLException {
-        StatementStrategy statementStrategy = new AddStatement(user);
-        jdbcContextWithStatementStrategy(statementStrategy);
+        jdbcContextWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement(
+                        "insert into users(id, name, password) values(?, ?, ?)");
+                
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
+                
+                return ps;
+            }
+        });
     }
 
     public User get(String id) throws SQLException {
@@ -53,8 +64,11 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        StatementStrategy statementStrategy = new DeleteAllStatement();
-        jdbcContextWithStatementStrategy(statementStrategy);
+        jdbcContextWithStatementStrategy(new StatementStrategy() {
+            public PreparedStatement makeStatement(Connection c) throws SQLException {
+                return c.prepareStatement("delete from users");
+            }
+        });
     }
 
     public void jdbcContextWithStatementStrategy(StatementStrategy statementStrategy) throws SQLException{
