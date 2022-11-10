10d61e91d7212b80fc66fa0f4c330cd604f5238b
emadam <ldy9037@naver.com>
2022-11-10 12:41:14 +0900

    refactor(UserDao.java): context 메서드를 외부 class로 분리
    - 다른 DAO에서도 context 메서드를 사용할 수 있도록 JdbcContext라는 이름의 class로 분리.
    - UserDao에서는 JdbcContext를 주입받아 사용하기 위해 setter를 생성.
    - applicationContext.xml에 의존관계 설정 추가
    
    토비의 스프링 231 - 235

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -14,13 +14,18 @@ import springbook.user.domain.User;
 public class UserDao {
 
     private DataSource dataSource;
+    private JdbcContext jdbcContext;
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
+    public void setJdbcContext(JdbcContext jdbcContext) {
+        this.jdbcContext = jdbcContext;
+    }
+
     public void add(User user) throws SQLException {
-        jdbcContextWithStatementStrategy(new StatementStrategy() {
+        jdbcContext.workWithStatementStrategy(new StatementStrategy() {
             @Override
             public PreparedStatement makeStatement(Connection c) throws SQLException {
                 PreparedStatement ps = c.prepareStatement(
@@ -64,39 +69,11 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        jdbcContextWithStatementStrategy(new StatementStrategy() {
+        jdbcContext.workWithStatementStrategy(new StatementStrategy() {
             public PreparedStatement makeStatement(Connection c) throws SQLException {
                 return c.prepareStatement("delete from users");
             }
         });
-    }
-
-    public void jdbcContextWithStatementStrategy(StatementStrategy statementStrategy) throws SQLException{
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = dataSource.getConnection();
-            
-            ps = statementStrategy.makeStatement(c);
-
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
     }
 
     public int getCount() throws SQLException {
