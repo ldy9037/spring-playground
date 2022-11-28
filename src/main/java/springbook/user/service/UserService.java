0aac3ff80bbd881bb2375a1f313a73d80d663e96
emadam <ldy9037@naver.com>
2022-11-28 12:42:39 +0900

    refactor(canUpgradeLevel): Level 업그레이드 조건 값을 상수로 변경
    업그레이드 조건 값 중복을 제거하기 위해 상수로 분리. SILVER 업그레이드에 대한 조건은 값은 MIN_LOGCOUNT_FOR_SILVER, GOLD 업그레이드에 대한 조건 값은 MIN_RECCOMEND_FOR_GOID로 지정
    
    토비의 스프링 347 - 348

--- src/main/java/springbook/user/service/UserService.java
+++ src/main/java/springbook/user/service/UserService.java
@@ -7,6 +7,10 @@ import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
 public class UserService {
+    
+    public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
+    public static final int MIN_RECCOMEND_FOR_GOLD = 30;
+    
     UserDao userDao;
 
     public void setUserDao(UserDao userDao) {
@@ -35,8 +39,8 @@ public class UserService {
         Level currentLevel = user.getLevel();
 
         switch (currentLevel) {
-            case BASIC: return (user.getLogin() >= 50);
-            case SILVER: return (user.getRecommend() >= 30);
+            case BASIC: return (user.getLogin() >= MIN_LOGCOUNT_FOR_SILVER);
+            case SILVER: return (user.getRecommend() >= MIN_RECCOMEND_FOR_GOLD);
             case GOLD: return false;
             default: throw new IllegalArgumentException("Unknown Level: " + currentLevel);
         }
--- src/test/java/springbook/user/service/UserServiceTest.java
+++ src/test/java/springbook/user/service/UserServiceTest.java
@@ -31,11 +31,11 @@ public class UserServiceTest {
     @BeforeEach
     void setUp() {
         users = new ArrayList<>();
-        users.add(new User("bumjin", "박범진", "p1", Level.BASIC, 49, 0));
-        users.add(new User("joytouch", "강명성", "p2", Level.BASIC, 50, 0));
-        users.add(new User("erwins", "신승환", "p3", Level.SILVER, 60, 29));
-        users.add(new User("madnite1", "이상호", "p4", Level.SILVER, 60, 30));
-        users.add(new User("green", "오민규", "p5", Level.GOLD, 100, 100));
+        users.add(new User("bumjin", "박범진", "p1", Level.BASIC, UserService.MIN_LOGCOUNT_FOR_SILVER - 1, 0));
+        users.add(new User("joytouch", "강명성", "p2", Level.BASIC, UserService.MIN_LOGCOUNT_FOR_SILVER, 0));
+        users.add(new User("erwins", "신승환", "p3", Level.SILVER, 60, UserService.MIN_RECCOMEND_FOR_GOLD - 1));
+        users.add(new User("madnite1", "이상호", "p4", Level.SILVER, 60, UserService.MIN_RECCOMEND_FOR_GOLD));
+        users.add(new User("green", "오민규", "p5", Level.GOLD, 100, Integer.MAX_VALUE));
     }
 
     @Test
@@ -47,11 +47,11 @@ public class UserServiceTest {
 
         userService.upgradeLevels();
 
-        checkLevel(users.get(0), Level.BASIC);
-        checkLevel(users.get(1), Level.SILVER);
-        checkLevel(users.get(2), Level.SILVER);
-        checkLevel(users.get(3), Level.GOLD);
-        checkLevel(users.get(4), Level.GOLD);    
+        checkLevelUpgraded(users.get(0), false);
+        checkLevelUpgraded(users.get(1), true);
+        checkLevelUpgraded(users.get(2), false);
+        checkLevelUpgraded(users.get(3), true);
+        checkLevelUpgraded(users.get(4), false);    
     }
 
     @Test
@@ -75,5 +75,14 @@ public class UserServiceTest {
     private void checkLevel(User user, Level expectedLevel) {
         User userUpdate = userDao.get(user.getId());
         assertThat(userUpdate.getLevel()).isEqualTo(expectedLevel);
+    }
+
+    private void checkLevelUpgraded(User user, boolean upgraded) {
+        User userUpdate = userDao.get(user.getId());
+        if (upgraded) {
+            assertThat(userUpdate.getLevel()).isEqualTo(user.getLevel().nextLevel());
+        } else {
+            assertThat(userUpdate.getLevel()).isEqualTo(user.getLevel());
+        }
     }
 }
